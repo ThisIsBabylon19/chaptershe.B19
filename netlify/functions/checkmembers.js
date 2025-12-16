bb92355ce1a67b76560e22cec82aad0e222a55ec
Madam Ella <madamellamalala@gmail.com>
2025-12-16 15:26:32 +0200

    ,

--- netlify/functions/checkmembers.js
+++ netlify/functions/checkmembers.js
@@ -12,7 +12,7 @@ exports.handler = async (event) => {
     }
 
     const client = new Client({
-      connectionString: process.env.DATABASE_URL,
+      connectionString: process.env.NETLIFY_DATABASE_URL,
       ssl: { rejectUnauthorized: false }
     });
 
